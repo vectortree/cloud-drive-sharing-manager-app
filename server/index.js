0d6f0106fdd1d003735066692203d04e7ae4c6a3
Starr Xu <sxu@pop-os.localdomain>
2022-10-08 18:31:53 -0400

    Basic server setup

--- server/index.js
+++ server/index.js
@@ -1,50 +1,20 @@
-const http = require('http');
 const express = require('express');
-const socketio = require('socket.io');
 const cors = require('cors');
-
-//const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
-//const router = require('./router');
-
 const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
+const mongoose = require("mongoose");
 
 app.use(cors());
-//app.use(router);
+app.use(express.json());
 
-io.on('connect', (socket) => {
-    socket.on('join', ({ name, room }, callback) => {
-        const { error, user } = addUser({ id: socket.id, name, room });
 
-        if(error) return callback(error);
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
 
-        socket.join(user.room);
-
-        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to cse416Proejct ${user.room}.`});
-        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
-
-        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
-
-        callback();
-    });
-
-    socket.on('sendMessage', (message, callback) => {
-        const user = getUser(socket.id);
-
-        io.to(user.room).emit('message', { user: user.name, text: message });
-
-        callback();
-    });
-
-    socket.on('disconnect', () => {
-        const user = removeUser(socket.id);
-
-        if(user) {
-            io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
-            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
-        }
-    })
+app.listen(port, () => {
+    // Perform a database connection when server starts
+    mongoose
+    .connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("Successfully connected to MongoDB."))
+    .catch((err) => console.log(err));
+    console.log(`Server is running on port: ${port}`);
 });
-
-server.listen(process.env.PORT || 8080, () => console.log(`Server has started.`));
