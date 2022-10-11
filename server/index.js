a4be6cdf4e86d3edc746f6965b0915e0e733eb02
Starr Xu <sxu@pop-os.localdomain>
2022-10-11 15:11:31 -0400

    Small fix: CLIENT_ORIGIN -> CLIENT_URL

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors({
-    origin: process.env.CLIENT_ORIGIN,
+    origin: process.env.CLIENT_URL,
     methods: "GET, POST, PUT, DELETE",
     credentials: true}));
 
