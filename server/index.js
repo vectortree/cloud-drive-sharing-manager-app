a4f0c06535ce2024ca46bac1a929dfee2d16c504
Starr Xu <sxu@pop-os.localdomain>
2022-10-31 23:44:13 -0400

    Fixed typo

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//const compression = require('compression');
+const compression = require('compression');
 const mongoose = require("mongoose");
 const cors = require('cors');
 require("dotenv").config();
@@ -12,7 +12,7 @@ require('./passport/passport-microsoft');
 
 const port = process.env.PORT || 5001;
 
-//app.use(compression());
+app.use(compression());
 app.use(express.json());
 
 app.use(cors({
