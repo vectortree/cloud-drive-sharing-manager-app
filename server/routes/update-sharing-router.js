d3a803cb3f971bb0d0323dcddb1584574ac9a650
Starr Xu <starr.xu@stonybrook.edu>
2022-11-28 23:32:46 -0500

    Update sharing: Unshare files backend route

--- server/routes/update-sharing-router.js
+++ server/routes/update-sharing-router.js
@@ -270,9 +270,105 @@ router.post('/removepermission', async (req, res) => {
     });
 });
 
-// TODO
 router.post('/unsharefiles', async (req, res) => {
     // Note: Front-end should only send files in MyDrive
+    if(!req.user) return res.status(401).json({success: false, message: "Error"});
+
+    UserProfile.findById(req.user._id, async (err, userProfile) => {
+        if(err) console.log(err);
+        if(err || !userProfile) return res.status(500).json({success: false, message: "Error"});
+        const { files } = req.body;
+
+        if(!files) return res.status(400).json({success: false, message: "Invalid data format"});
+        if(files.length == 0) return res.status(400).json({success: false, message: "No files specified"});
+        if(userProfile.fileSharingSnapshots.length == 0) return res.status(400).json({success: false, message: "No file-sharing snapshot in user profile"});
+        if(userProfile.user.driveType === "google") {
+            // Make sure to refresh access token before attempting to access Google Drive API
+            if(userProfile.user.tokens.refresh_token) {
+                refresh.requestNewAccessToken(
+                    'google',
+                    userProfile.user.tokens.refresh_token,
+                    function (err, accessToken, refreshToken) {
+                        // Store new tokens in database
+                        userProfile.user.tokens.access_token = accessToken;
+                        userProfile.user.tokens.refresh_token = refreshToken;
+                        userProfile.save();
+                    },
+                );
+            }
+            const oAuth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID,
+                process.env.GOOGLE_CLIENT_SECRET, process.env.CLIENT_URL);
+            
+            oAuth2Client.setCredentials(userProfile.user.tokens);
+            // Create Google Drive object to call API
+            const googleDrive = google.drive({
+                version: 'v3',
+                auth: oAuth2Client
+            });
+            try {
+                for(const file of files) {
+                    // A user is only authorized to make permission requests for files that include a permissions array
+                    if(file.permissions) {
+                        // If permission has a non-"owner" role in file.permissions (i.e., file is shared),
+                        // then make an API call to remove permission for file
+                        let shared = false;
+                        for(const permission of file.permissions) {
+                            if(permission.role !== "owner") {
+                                await googleDrive.permissions.delete({
+                                    fileId: file.id,
+                                    permissionId: permission.id
+                                });
+                                shared = true;
+                            }
+                        }
+                        if(shared) {
+                            let updatedPermissions = await getPermissionsGoogle(googleDrive, file.id);
+                            // Find file in most recent file-sharing snapshot and update its permissions
+                            userProfile.fileSharingSnapshots[userProfile.fileSharingSnapshots.length - 1].data.forEach((f, index) => {
+                                if(f.id === file.id)
+                                    userProfile.fileSharingSnapshots[userProfile.fileSharingSnapshots.length - 1].data[index].permissions = updatedPermissions;
+                            });
+                            if(file.mimeType === 'application/vnd.google-apps.folder' && file.path) {
+                                // If file is a folder, get file IDs of all files under it, make API calls to get permissions for each file,
+                                // and update each file's permissions
+                                let fileIds = getFilesIdsUnderFolder(userProfile.fileSharingSnapshots[userProfile.fileSharingSnapshots.length - 1].data, file.path + '/' + file.name, file.id);
+                                for(const id of fileIds) {
+                                    updatedPermissions = await getPermissionsGoogle(googleDrive, id);
+                                    // Find file in most recent file-sharing snapshot and update its permissions
+                                    userProfile.fileSharingSnapshots[userProfile.fileSharingSnapshots.length - 1].data.forEach((f, index) => {
+                                        if(f.id === id)
+                                            userProfile.fileSharingSnapshots[userProfile.fileSharingSnapshots.length - 1].data[index].permissions = updatedPermissions;
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+                // Log sharing changes
+                userProfile.sharingChangesLog.push({
+                    files: files,
+                    permissionType: 'n/a',
+                    permissionRole: 'n/a',
+                    permissionValue: 'n/a',
+                    action: 'unshare'
+                });
+            } catch(err) {
+                console.log(err);
+                return res.status(500).json({success: false, message: "Error"});
+            }
+        }
+        // TODO
+        else if(userProfile.user.driveType === "microsoft") {
+        }
+        // Save to database
+        try {
+            await userProfile.save();
+        } catch(err) {
+            console.log(err);
+            return res.status(500).json({success: false, message: "Error"});
+        }
+        return sendUserProfile(res, userProfile);
+    });
 
 });
 
