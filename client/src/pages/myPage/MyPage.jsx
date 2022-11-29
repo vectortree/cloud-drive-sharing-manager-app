bdb868cc43fe561b0ce37e79cae6697054cb0f59
Singularity2050 <67400401+Singularity2050@users.noreply.github.com>
2022-11-29 05:33:48 -0500

    fix bug

--- client/src/pages/myPage/MyPage.jsx
+++ client/src/pages/myPage/MyPage.jsx
@@ -79,8 +79,10 @@ const MyPage = (props)=>{
             queryString += SearchQuery[i].searchQuery.operator + ":"+SearchQuery[i].searchQuery.argument+"}"
         }else if(SearchQuery[i].searchQuery.children){
             const childrenLength = SearchQuery[i].searchQuery.children.length
-            for(let j = 0; j < childrenLength.length; j++){
-                queryString += SearchQuery[i].searchQuery.children[j].operator + ":"+SearchQuery[i].searchQuery.children[j].argument+" "+ SearchQuery[i].searchQuery.logicalOp;
+            console.log(childrenLength);
+            for(let j = 0; j < childrenLength   -1; j++){
+
+                queryString += SearchQuery[i].searchQuery.children[j].operator + ":"+SearchQuery[i].searchQuery.children[j].argument+" "+ SearchQuery[i].searchQuery.logicalOp+ " ";
             }
             console.log(queryString);
             queryString += SearchQuery[i].searchQuery.children[childrenLength-1].operator + ":"+SearchQuery[i].searchQuery.children[childrenLength-1].argument+"}";
